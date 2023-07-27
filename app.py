d7120b4ba9a3b53937eef4f9639bf511bd4e0262
Aleksejs Nazarovs <104507318+nazalexx@users.noreply.github.com>
2023-07-27 20:06:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
+from flask import Flask, request, render_template, redirect, url_for
+import json
+import os
+
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def get_datetime():
+    return f'Hello world!'
+
+
+
+@app.route('/calculator')
+def get_datetime():
+    return f'Hello calculator!'
+
+
+
+@app.route('/results')
+def get_datetime():
+    return f'Hello results!'
+
 
