14bcc1ed9ea4aa3899874e630df90a673c6cd72e
Aleksejs Nazarovs <104507318+nazalexx@users.noreply.github.com>
2023-07-28 00:19:41 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,12 +6,12 @@ with open('columns.json', 'r') as file:
 def user_inputs_to_model_input(inputs):
     model_input = []
     for col in columns[1:]:
-        if col.split(' / ')[0] in choosable_features:
+        if len(col.split(' / ')) > 1:
             if col.split(' / ')[1]==inputs[col.split(' / ')[0]]:
                 model_input.append(1)
             else:
                 model_input.append(0)
-        elif col.split(' / ')[0] in nonbinary_parameters:
-            model_input.append(inputs[col.split(' / ')[0]])
-    model_input = pd.DataFrame([model_input], columns=columns[1:])
+        else:
+            model_input.append(inputs[col])
+    model_input = DataFrame([model_input], columns=columns[1:])
     return model_input
