21e7db65d2a6294305934c9e0a2072a9772c4498
Aleksejs Nazarovs <104507318+nazalexx@users.noreply.github.com>
2023-07-28 00:15:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, render_template, redirect, url_for
 import json
 import os
+import utils
+
 
 
 app = Flask(__name__)
+
 
 
 @app.route('/')
@@ -11,9 +14,55 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/calculator')
-def calculator():
-    return render_template('calculator.html')
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    
+    with open('allowed_choices.json', 'r') as file:
+        allowed_choices = json.load(file)
+    with open('columns.json', 'r') as file:
+        columns = json.load(file)
+    with open('col_descriptions.json', 'r') as file:
+        col_descriptions = json.load(file)
+    with open('inefficient_inputs.json', 'r') as file:
+        inefficient_inputs = json.load(file)
+    with open('average_inputs.json', 'r') as file:
+        average_inputs = json.load(file)
+    with open('efficient_inputs.json', 'r') as file:
+        efficient_inputs = json.load(file)
+    enterable_features = [col for col in columns[1:] if col not in allowed_choices]
+
+    return render_template('calculator.html', 
+                           allowed_choices = allowed_choices, 
+                           enterable_features = enterable_features, 
+                           inefficient_inputs = inefficient_inputs, 
+                           average_inputs = average_inputs, 
+                           efficient_inputs = efficient_inputs,
+                           default_inputs = None if request.method == 'GET' else request.form.get('default_inputs')
+                          )
+
+
+
+@app.route('/results/<user>', methods=['GET', 'POST'])
+def results(user):
+
+    if request.method == 'GET':
+        args = request.args
+        if 'user' in args.keys():
+            user = args['user']
+            # Load USER info
+            if user:
+                return render_template('result.html', user=user)
+            else:
+                return f'No results for {user} in our database :('
+        else:
+            # Load all users
+            return ''
+
+    elif request.method == 'POST':
+        args = request.form
+        user = args['user']
+        for key, value
 
 
 @app.route('/results')
